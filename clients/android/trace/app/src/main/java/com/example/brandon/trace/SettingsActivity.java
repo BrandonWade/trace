ccc2f292b79a2803b6acf39ad93665a605073e2f
Brandon Wade <brandon_wade2005@hotmail.com>
2016-12-18 21:01:03 -0600

    Wire up max connections seek bar to connections label.

--- clients/android/trace/app/src/main/java/com/example/brandon/trace/SettingsActivity.java
+++ clients/android/trace/app/src/main/java/com/example/brandon/trace/SettingsActivity.java
@@ -5,8 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private int numConnections = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,24 @@ public class SettingsActivity extends AppCompatActivity {
 
         EditText addressField = (EditText) findViewById(R.id.storage_directory_field);
         addressField.setText(serverAddress);
+
+        final TextView numConnectionsLabel = (TextView)findViewById(R.id.connections_label);
+        final SeekBar maxConnectionsBar = (SeekBar)findViewById(R.id.max_connections_slider);
+        numConnectionsLabel.setText(String.valueOf(numConnections));
+
+        maxConnectionsBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                numConnections = (progress + 1);
+                numConnectionsLabel.setText(String.valueOf(numConnections));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
 
     public void setStorageDirectory(View view) {
