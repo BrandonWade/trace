ca6468594dca453992d148e523199b1827068bad
Brandon Wade <brandon_wade2005@hotmail.com>
2017-03-21 19:07:52 -0500

    Restructuring, add support for basic CSS.

--- server/app/index.js
+++
@@ -1,7 +0,0 @@
-import m from 'mithril';
-
-const root = document.getElementById('root');
-
-m.render(root,
-  m('h1', 'Hello world!'),
-);
---
+++ server/src/app/app.js
@@ -0,0 +1,8 @@
+import m from 'mithril';
+import './app.css';
+
+const body = document.body;
+
+m.render(body,
+  m('h1', 'Hello world!'),
+);
