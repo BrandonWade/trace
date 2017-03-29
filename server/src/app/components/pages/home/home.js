83512ce39fd9b065a3c0c352979b491e9709862e
Brandon Wade <brandon_wade2005@hotmail.com>
2017-03-29 15:55:48 -0500

    Add basic Pages and routing between them.

---
+++ server/src/app/components/pages/home/home.js
@@ -0,0 +1,11 @@
+import m from 'mithril';
+
+const Home = {
+  view() {
+    return m('.HomePage',
+      m('h1.PageHeading', 'Home'),
+    );
+  },
+};
+
+export default Home;
---
+++ server/src/app/components/pages/settings/settings.js
@@ -0,0 +1,11 @@
+import m from 'mithril';
+
+const Settings = {
+  view() {
+    return m('.SettingsPage',
+      m('h1.PageHeading', 'Settings'),
+    );
+  },
+};
+
+export default Settings;
