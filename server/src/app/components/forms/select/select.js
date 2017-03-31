dc67e2ae610fa9d9428e3900c642fc1bd01eb95f
Brandon Wade <brandon_wade2005@hotmail.com>
2017-03-30 20:54:50 -0500

    Begin adding controls and logic to pages.

---
+++ server/src/app/components/forms/select/select.js
@@ -0,0 +1,25 @@
+import m from 'mithril';
+
+const Select = {
+  oninit(vn) {
+    this.description = vn.attrs.description || '';
+    this.options = vn.attrs.options || [];
+    this.size = Math.max(this.options.length, 10);
+    this.onchange = vn.attrs.onchange;
+  },
+
+  view() {
+    return m('.SelectContainer', [
+      m('label.SelectLabel', m.trust(this.description)),
+      m('select.Select', {
+        size: this.size,
+        multiple: this.multiple,
+        onchange: this.onchange,
+      }, [
+        this.options.map(option => m('option', option))
+      ]),
+    ]);
+  },
+};
+
+export default Select;
--- server/src/app/components/pages/filters/filters.js
+++ server/src/app/components/pages/filters/filters.js
@@ -1,9 +1,50 @@
 import m from 'mithril';
+import stream from 'mithril/stream';
+import TextBox from '../../forms/textbox/textbox';
+import Button from '../../forms/button/button';
+import Select from '../../forms/select/select';
 
 const Filter = {
+  saveFilters() {
+    localStorage.setItem('trace.filters.ignore', JSON.stringify(this.filters()));
+  },
+
+  oninit() {
+    const filters = JSON.parse(localStorage.getItem('trace.filters.ignore')) || [];
+    this.filters = stream(filters);
+    this.newFilter = stream();
+    this.selectedFilterIndex = stream();
+  },
+
   view() {
     return m('.FiltersPage',
       m('h1.PageHeading', 'Filters'),
+      m(TextBox, {
+        description: 'New filter:',
+        onchange: e => this.newFilter(e.target.value),
+      }),
+      m(Button, {
+        value: 'Add',
+        onclick: () => {
+          this.filters().push(this.newFilter());
+          this.newFilter('');
+          this.saveFilters();
+        },
+      }),
+      m(Select, {
+        description: 'Existing filters:',
+        options: this.filters(),
+        onchange: e => this.selectedFilterIndex(e.target.selectedIndex),
+      }),
+      m(Button, {
+        value: 'Remove',
+        onclick: () => {
+          if (this.filters().length > 0) {
+            this.filters().splice(this.selectedFilterIndex(), 1);
+            this.saveFilters();
+          }
+        },
+      })
     );
   },
 };
