4f2ff0359fbda3a411752638f382ed3296f9da85
Brandon Wade <brandon_wade2005@hotmail.com>
2017-05-11 00:38:24 -0500

    Fix client memory leak with v2.5.

--- client/trace/app/src/main/java/com/example/brandon/trace/FileDownloadManager.java
+++ client/trace/app/src/main/java/com/example/brandon/trace/FileDownloadManager.java
@@ -1,6 +1,5 @@
 package com.example.brandon.trace;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Semaphore;
 
@@ -10,13 +9,11 @@ import java.util.concurrent.Semaphore;
 public class FileDownloadManager extends Thread {
 
     private boolean downloading;
-    private List<FileConnection> fileConnections;
 
     private static FileDownloadManager fdm;
 
     protected FileDownloadManager() {
         this.downloading = false;
-        this.fileConnections = new ArrayList<>();
     }
 
     public static FileDownloadManager getInstance() {
@@ -39,18 +36,11 @@ public class FileDownloadManager extends Thread {
         }
 
         for (int i = 0; i < files.size(); i++) {
-            if (this.isInterrupted()) {
-                cancel();
-                return;
-            }
-
             try {
                 lock.acquire();
 
                 FileConnection conn = new FileConnection(lock, files.get(i).fileName);
                 conn.start();
-
-                fileConnections.add(conn);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -74,14 +64,6 @@ public class FileDownloadManager extends Thread {
 
     public void run() {
         download();
-    }
-
-    public void cancel() {
-        for (FileConnection conn: fileConnections) {
-            if (!conn.isInterrupted()) {
-                conn.disconnect();
-            }
-        }
     }
 
     public boolean isDownloading() {
