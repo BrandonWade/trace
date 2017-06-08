16fd1a50ba3b9339aee5b2e1a5db5c5885fb75be
Brandon Wade <brandon_wade2005@hotmail.com>
2017-06-07 21:49:55 -0500

    Server UI updates, update android styling.

--- client/trace/app/src/main/java/com/example/brandon/trace/FileListItemAdapter.java
+++ client/trace/app/src/main/java/com/example/brandon/trace/FileListItemAdapter.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
